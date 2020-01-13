7c08a54967197370189d6136d2bc191b778ab18e
saasey <48605836+saasey@users.noreply.github.com>
2020-01-13 03:33:13 -0500

    v0.0.1a

--- index.php
+++ index.php
@@ -2,23 +2,25 @@
 
 $number = 82;
 $side = ""; //decbin($number);
-function compress_1(int $number, string &$side)
+function compress_1(int $number, string $side)
 {
-    $save = 0;
-    echo $number . " ";
+    $save = "";
+    //echo $number . " ";
     $binary = decbin($number);
-    
     if ($number == 0) {
         return;
     }
+    $side ="";
     $i = strlen($binary) - 1;
-    echo $binary . " ";
+    //$side = $binary[$i];
     while ($i > 0) {
-        $save = ($save << 1);
-        $bool = ($binary[$i] ^ $binary[$i - 1]);
-        $save = $save + (int)$bool;
+        //$save = ($save << 1);
+        $bool = (int)($binary[$i] XOR $binary[$i - 1]);
+        $side .= $bool;
         $i--;
     }
+    echo $side . " ";
+    //$number = bindec($save);
     $number = $number >> 1;
     
     compress_1($number, $side);
