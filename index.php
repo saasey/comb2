bba3b49f651f42988eb996cd107181919bab8178
saasey <48605836+saasey@users.noreply.github.com>
2020-01-13 15:11:04 -0500

    v0.0.1d

--- index.php
+++ index.php
@@ -1,51 +1,52 @@
 <?php
-$saved_side = "";
-$number = 47;
-$side = "";
-echo decbin($number) . " ";
-function compress_1(int $number, string $level = "")
+
+$number = 12431;
+$side = ""; //decbin($number);
+function compress_1(int $number, string $side, string $save = "")
 {
     //echo $number . " ";
-    $binary = decbin($number);
-    
-    if (strlen($binary) < 3) {
-        return array($level, $binary);
+    if ($side == "")
+        $binary = decbin($number);
+    else
+        $binary = $side;
+    if (strlen($binary) < 4) {
+        return array($save . $binary[0], $binary);
     }
-    //$save = $binary[0] . $save;
-    $i = 0; //strlen($binary) - 1;
-    $level = ($binary[0]);
-    while ($i < strlen($binary) - 1) {
-        $bool = ($binary[$i] XOR $binary[$i + 1]);
-        $level .= $level[strlen($level)-1] XOR $bool;
-        $i++;
+    $save .= $binary[0];
+    $side ="";
+    $i = strlen($binary) - 1;
+    //$side = $binary[$i];
+    while ($i > 0) {
+        //$save = ($save << 1);
+        $bool = (int)($binary[$i] XOR $binary[$i - 1]);
+
+        $side .= $bool;
+        $i--;
     }
-    echo $level . "%";
-    $number = bindec($level);
+    echo $side . " ";
+    $number = bindec($side);
     $number = $number >> 1;
     
-    $output = compress_1($number, substr($level,0));
-    return $output;
+    return compress_1($number, $side, $save);
 }
 
-$comp = compress_1($number);
+$comp = compress_1($number, $side);
 echo "<br/>";
 
 function decompress(string $save, string $decomp) {
-    if (strlen($save) == 1)
-        return substr($decomp,0);
+    
     // add to side (1 bool)
-    $decomp = $save[0] . $decomp;
-    $i = strlen($decomp) - 1;
-    $comp = (string)($save[0] XOR $decomp[0]);
-    while ($i > 0) {
-        $bool = $decomp[$i] XOR $decomp[$i - 1];
-        $comp .= $comp[strlen($comp)-1] XOR $bool;
-        //echo $comp;
-        $i--;
+    $i = 0;
+    $comp = $save[0];
+    while ($i < strlen($decomp)) {
+        $bool = $decomp[strlen($decomp) - $i - 1] XOR $decomp[strlen($decomp) - $i - 2];
+        $comp .= (string)($bool);
+        $i++;
     }
     echo $comp . " ";
-    $decomp = decompress(substr($save,1), $comp);
-    return $decomp;
+    if (strlen($save) == 1)
+        return $comp;
+    return decompress(substr($save,1), $comp);
 }
 $decomp = decompress($comp[0], $comp[1]);
 echo $decomp;
