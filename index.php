ebd4bf05dfdd5a9c29704875f933cb42b24dbbc4
saasey <48605836+saasey@users.noreply.github.com>
2020-01-13 13:04:19 -0500

    v0.0.1c included non-working decompressor

--- index.php
+++ index.php
@@ -1,35 +1,52 @@
 <?php
-
-$number = 243;
-$side = ""; //decbin($number);
-function compress_1($number, string $side)
+$saved_side = "";
+$number = 47;
+$side = "";
+echo decbin($number) . " ";
+function compress_1(int $number, string $level = "")
 {
-    $save = "";
     //echo $number . " ";
-    if ($side == "")
-        $binary = decbin($number);
-    else
-        $binary = $side;
+    $binary = decbin($number);
+    
     if (strlen($binary) < 3) {
-        return;
+        return array($level, $binary);
     }
-    $side ="";
-    $i = strlen($binary) - 1;
-    //$side = $binary[$i];
-    while ($i > 0) {
-        //$save = ($save << 1);
-        $bool = (int)($binary[$i] XOR $binary[$i - 1]);
-
-        $side .= $bool;
-        $i--;
+    //$save = $binary[0] . $save;
+    $i = 0; //strlen($binary) - 1;
+    $level = ($binary[0]);
+    while ($i < strlen($binary) - 1) {
+        $bool = ($binary[$i] XOR $binary[$i + 1]);
+        $level .= $level[strlen($level)-1] XOR $bool;
+        $i++;
     }
-    echo $side . " ";
-    $number = bindec($side);
+    echo $level . "%";
+    $number = bindec($level);
     $number = $number >> 1;
     
-    compress_1($number, $side);
+    $output = compress_1($number, substr($level,0));
+    return $output;
 }
 
-compress_1($number, $side);
+$comp = compress_1($number);
+echo "<br/>";
 
+function decompress(string $save, string $decomp) {
+    if (strlen($save) == 1)
+        return substr($decomp,0);
+    // add to side (1 bool)
+    $decomp = $save[0] . $decomp;
+    $i = strlen($decomp) - 1;
+    $comp = (string)($save[0] XOR $decomp[0]);
+    while ($i > 0) {
+        $bool = $decomp[$i] XOR $decomp[$i - 1];
+        $comp .= $comp[strlen($comp)-1] XOR $bool;
+        //echo $comp;
+        $i--;
+    }
+    echo $comp . " ";
+    $decomp = decompress(substr($save,1), $comp);
+    return $decomp;
+}
+$decomp = decompress($comp[0], $comp[1]);
+echo $decomp;
 ?>
