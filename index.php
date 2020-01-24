eb01671ccd57db9ac9a1106a6a0eb8e029ba7ae0
saasey <48605836+saasey@users.noreply.github.com>
2020-01-23 23:54:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,59 @@
 <?php
 
-$number = 124310;
+$number = 222910;
 $side = ""; //decbin($number);
-function compress_1(int $number, string $side = "", string $save = "")
+function compress_1(string $side, string $save = "")
 {
     //echo $number . " ";
-    if ($side == "")
-        $binary = decbin($number);
-    else
-        $binary = $side;
-    if (strlen($binary) <= 8) {
-        return array($save, $binary);
+    $binary = ($side);
+
+    if (strlen($binary) <= 4) {
+        return array(strrev($save), strrev($binary));
     }
     // save will come out backward
 
-    
-    $side = ""; //decbin($save[strlen($save)-1]);
-    $i = 0;
+    //$side .= decbin($save[strlen($save)-1]);
+    $i = 1;
+    $side2 = "";
+    while (strlen($side2) < strlen($binary)-1) {
 
-    while ($i < strlen($binary) - 1) {
+        $side2 .= decbin($binary[$i-1] xor $binary[$i]);
 
-        $bool = decbin($binary[$i] XOR $binary[$i + 1]);
-        
-        $side = decbin($bool) . $side;
-        
         $i++;
-    
+
     }
-    $save .= $side[0];
-    echo " a" . $side . "b ";
-    $number = bindec($side); 
-    $number = $number >> 1;
-    
-    return compress_1($number, $side, $save);
+
+    $save .= (string)($binary[0]);
+    echo " c" . $side2 . "d ";
+    $temp = substr($side2, 1, strlen($side2)-1);
+
+    return compress_1($side2, $save);
 }
-
+echo decbin($number) . "<br/>";
 $comp = compress_1($number);
-echo "<br/>$comp[1]<br/>";
+echo "<br/>$comp[1]okcd<br/><br/>kcad$comp[0]<br/>";
 
-function decompress(string $save, string $binary) {
-    
-    // add to side (1 bool)
-    $i = 0;
-    $comp = "";
+function decompress(string $save, string $binary, bool $cont = false)
+{
+    $save = strrev($save);
+    $side = 0;
     if (strlen($save) > 0)
-        $comp = decbin($save[0]);
-    else
-        return $comp;
-    
-    while ($i < strlen($binary)) {
+    $binary .= $save[0];
+    $i = 0; // strlen($binary) - 1;
+    while (strlen($side) < strlen($binary)) {
 
-        $bool = decbin($binary[strlen($binary) - $i - 2] XOR $binary[strlen($binary) - $i - 1]);
-        
-        $comp .= decbin($bool);
-        
+        $side = decbin($binary[$i] xor $binary[$i + 1]) . $side;
+
         $i++;
-    
+
     }
-    
-    echo " a" . $comp . "b ";
-    if (strlen($save) == 1)
-        return $comp;
-    return decompress(substr($save,1,strlen($save)), $comp);
+    //if (strlen($save) > 0)
+    //    $side .= $save[0];
+    echo " a" . $side . "b ";
+    if (strlen($save) == 0) {
+        return $side;
+    }
+    return decompress(substr($save, 1), $side, true);
 }
 $decomp = decompress($comp[0], $comp[1]);
 echo $decomp;
-
-?>
